3ebdbab580f72d7292d411d10817d37cc5cce970
Beck <ulugbek_5755@mail.ru>
2023-04-23 21:16:56 +0200

    THE END

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let fields = [];
 
 let currentShape = 'cross';
 
+let gameOver = false;
 
 
 function fillShape(id) {
-    if(!fields[id]){ // !fields[id] wenn etwas hier drinnen ist dann führe untere Befehle aus ansonsten nicht. ! - true !! - false
+    if(!fields[id] && !gameOver/* fülle dieses Feld aus und wenn nicht gameOver ist. mit ! macht man true  */){ // !fields[id] wenn etwas hier drinnen ist dann führe untere Befehle aus ansonsten nicht. ! - true !! - false
     if(currentShape == 'cross'){ // currentShape = crossga
         currentShape = 'circle'; // unda circle ga uzgaradi
         document.getElementById('player-1').classList.remove('player-inactive');
@@ -21,6 +22,22 @@ function fillShape(id) {
     draw(); // damit der Spiel immer neue ladet
     checkForWin();
 }
+}
+
+function restart(){
+    gameOver = false;
+    fields = [];
+
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+
+    for(let i=1; i < 8; i++){
+        document.getElementById('line-' + i).classList.add('d-none');// bei line-1 entfernt und füge dazu i hinzu
+    }
+    for(let i=0; i < 9; i++){
+        document.getElementById('circle-' + i).classList.add('d-none');// bei line-1 entfernt und füge dazu i hinzu
+        document.getElementById('cross-' + i).classList.add('d-none');// bei line-1 entfernt und füge dazu i hinzu
+    }
 }
 
 function draw(){
@@ -40,46 +57,53 @@ function checkForWin(){
 
     if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
         winner = fields[0];
-        document.getElementById('line-1').style.transform = 'scaleX(1.25)'; // das ist für horizontale linie
+        document.getElementById('line-1').style.transform = 'scaleX(1.1)'; // das ist für horizontale linie
     }
 
     if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]){
         winner = fields[3];
-        document.getElementById('line-2').style.transform = 'scaleX(1.25)';
+        document.getElementById('line-2').style.transform = 'scaleX(1.1)';
     }
 
     if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
         winner = fields[6];
-        document.getElementById('line-3').style.transform = 'scaleX(1.25)';
+        document.getElementById('line-3').style.transform = 'scaleX(1.1)';
     }
 
     if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
         winner = fields[0];
-        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.25)';
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.1)';
     }
 
     if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
         winner = fields[1];
-        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.25)';
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.1)';
     }
 
     if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
         winner = fields[2];
-        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1.25)';
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1.1)';
     }
     if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
         winner = fields[0];
-        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.25)'; // das ist für vertikale linie
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.1)'; // das ist für vertikale linie
     }
     if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
         winner = fields[2];
-        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.25)'; // das ist für vertikale linie
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.1)'; // das ist für vertikale linie
     }
     if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
         winner = fields[6];
-        document.getElementById('line-9').style.transform = 'scaleX(1.25)';
+        document.getElementById('line-9').style.transform = 'scaleX(1.1)';
     }
     if(winner){
         console.log('GEWONNEN:', winner);
+        gameOver = true;
+
+        setTimeout(function(){ // ruft den function 1 sekunden später
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-btn').classList.remove('d-none');
+
+        }, 1000 );
     }
 }
